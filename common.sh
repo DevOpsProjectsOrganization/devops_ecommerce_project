9f76eaa1c282a498c75aad1236968e9cba3bda9f
Sridevi <parimisridevi586@gmail.com>
2025-06-27 21:29:01 +1000

    added functions to payments.sh and DRY to common.sh

--- common.sh
+++ common.sh
@@ -1,23 +1,10 @@
-NODEJS(){
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-useradd roboshop
-cp -f ${component}.service /etc/systemd/system/${component}.service
-rm -rf /app
-mkdir /app 
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip 
-cd /app 
-unzip /tmp/${component}.zip
-cd /app 
-npm install
+SYSTEMD(){
 systemctl daemon-reload
 systemctl enable ${component}
 systemctl start ${component}
 }
 
-MAVEN(){
-dnf install maven -y
+APP_PREREQ(){
 useradd roboshop
 cp -f ${component}.service /etc/systemd/system/${component}.service
 rm -rf /app
@@ -26,13 +13,39 @@ curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${c
 cd /app 
 unzip /tmp/${component}.zip
 cd /app 
+}
+
+NODEJS(){
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+APP_PREREQ
+npm install
+
+SYSTEMD
+}
+
+MAVEN(){
+dnf install maven -y
+
+APP_PREREQ
 mvn clean package 
 mv target/${component}-1.0.jar ${component}.jar  
 dnf install mysql -y 
 mysql -h mysql-dev.sdevops.shop -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h mysql-dev.sdevops.shop -uroot -pRoboShop@1 < /app/db/app-user.sql 
 mysql -h mysql-dev.sdevops.shop -uroot -pRoboShop@1 < /app/db/master-data.sql
-systemctl daemon-reload
-systemctl enable ${component}
-systemctl restart ${component}
+
+SYSTEMD
+}
+
+
+PYTHON(){
+dnf install python3 gcc python3-devel -y
+
+APP_PREREQ
+pip3 install -r requirements.txt
+
+SYSTEMD
 }
