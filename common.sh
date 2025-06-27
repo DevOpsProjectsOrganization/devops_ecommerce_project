b84e831eb4605347d89984ae07a0bf313acc5057
Sridevi <parimisridevi586@gmail.com>
2025-06-27 21:12:11 +1000

    applied functions in shipping.sh

--- common.sh
+++ common.sh
@@ -15,3 +15,24 @@ systemctl daemon-reload
 systemctl enable ${component}
 systemctl start ${component}
 }
+
+MAVEN(){
+dnf install maven -y
+useradd roboshop
+cp -f ${component}.service /etc/systemd/system/${component}.service
+rm -rf /app
+mkdir /app 
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip 
+cd /app 
+unzip /tmp/${component}.zip
+cd /app 
+mvn clean package 
+mv target/${component}-1.0.jar ${component}.jar  
+dnf install mysql -y 
+mysql -h mysql-dev.sdevops.shop -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql-dev.sdevops.shop -uroot -pRoboShop@1 < /app/db/app-user.sql 
+mysql -h mysql-dev.sdevops.shop -uroot -pRoboShop@1 < /app/db/master-data.sql
+systemctl daemon-reload
+systemctl enable ${component}
+systemctl restart ${component}
+}
--- shipping.sh
+++ shipping.sh
@@ -1,18 +1,3 @@
-dnf install maven -y
-useradd roboshop
-cp -f shipping.service /etc/systemd/system/shipping.service
-rm -rf /app
-mkdir /app 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
-cd /app 
-unzip /tmp/shipping.zip
-cd /app 
-mvn clean package 
-mv target/shipping-1.0.jar shipping.jar  
-dnf install mysql -y 
-mysql -h mysql-dev.sdevops.shop -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.sdevops.shop -uroot -pRoboShop@1 < /app/db/app-user.sql 
-mysql -h mysql-dev.sdevops.shop -uroot -pRoboShop@1 < /app/db/master-data.sql
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+source common.sh
+component=shipping
+MAVEN
