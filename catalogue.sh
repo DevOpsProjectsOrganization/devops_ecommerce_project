ac286630e58263bb416d27716e55314720a030cf
Sridevi <parimisridevi586@gmail.com>
2025-06-27 20:53:11 +1000

    replaced function in catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,8 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-useradd roboshop
-cp -f catalogue.service /etc/systemd/system/catalogue.service
 cp -f mongo.repo /etc/yum.repos.d/mongo.repo
-rm -rf /app
-mkdir /app 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
-cd /app 
-unzip /tmp/catalogue.zip
-cd /app 
-npm install
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
+
+source common
+component=catalogue
+NODEJS
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.sdevops.shop </app/db/master-data.js
---
+++ common.sh
@@ -0,0 +1,17 @@
+NODEJS(){
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd roboshop
+cp -f ${component}.service /etc/systemd/system/${component}.service
+rm -rf /app
+mkdir /app 
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip 
+cd /app 
+unzip /tmp/${component}.zip
+cd /app 
+npm install
+systemctl daemon-reload
+systemctl enable ${component}
+systemctl start ${component}
+}
