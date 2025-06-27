25bdc80cd3995ebcc85177b1d16f3ab2b6c44886
Sridevi <parimisridevi586@gmail.com>
2025-06-27 21:38:57 +1000

    added functions in dispatch.sh

--- dispatch.sh
+++ dispatch.sh
@@ -1,15 +1,8 @@
+source common.sh
+component=dispatch
 dnf install golang -y
-useradd roboshop
-cp -f dispatch.service /etc/systemd/system/dispatch.service
-rm -rf /app
-mkdir /app 
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip 
-cd /app 
-unzip /tmp/dispatch.zip
-cd /app 
+APP_PREREQ
 go mod init dispatch
 go get 
 go build
-systemctl daemon-reload
-systemctl enable dispatch 
-systemctl start dispatch
+SYSTEMD
