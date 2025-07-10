7945a527165dc0882e7b17afc42e10485edce0d8
Sridevi <parimisridevi586@gmail.com>
2025-07-10 20:49:30 +1000

    connecting ansible and creating all dns records along with the AWS instances and security groups

--- Phase_3_Terraform/modules/ec2/main.tf
+++ Phase_3_Terraform/modules/ec2/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "my_instance" {
     tags            = {
         Name        = "${var.name}-${var.env}"
     }
+    vpc_security_group_ids= [data.aws_security_group.selected.id]
 }
 
 resource "aws_route53_record" "my_public_record"{
@@ -11,5 +12,22 @@ resource "aws_route53_record" "my_public_record"{
     name    = "${var.name}-${var.env}"
     type    = "A"
     ttl     = 300
-    records = [aws_instance.my_instance.public_ip]
+    records = [aws_instance.my_instance.private_ip]
+}
+
+resource "null_resource" "ansible"{
+    depends_on = [aws_route53_record.my_public_record]
+    provisioner "remote-exec"{
+         connection {
+            type     = "ssh"
+            user     = "ec2-user"
+            password = "DevOps321"
+            host     = aws_instance.my_instance.private_ip
+        }
+        inline = [
+            "sudo pip3.11 install ansible hvac",
+            "ansible-pull -i localhost, -U https://github.com/DevOpsProjectsOrganization/devops_ecommerce_project/tree/main/Phase_2_Conf_Mgt_Ansible roboshop.yml -e role=$(var.name)"
+        ]
+       
+    }
 }
