ea5f99c5d932a75442762f9f5c7440d2b215280e
Sridevi <parimisridevi586@gmail.com>
2025-07-10 17:45:51 +1000

    first commit

--- Phase_3_Terraform/main.tf
+++ Phase_3_Terraform/main.tf
@@ -1,5 +1,6 @@
 module "ec2" {
     for_each        = var.instances
+    source          = "./modules/ec2"
     ami             = var.ami
     instance_type   =  each.value["instance_type"]
     zone_id         = var.zone_id
